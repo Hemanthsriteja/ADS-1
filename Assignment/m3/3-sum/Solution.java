f84d7c90b534feac01a2f4767ddccf64e27421d7
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-27 15:49:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ Assignment/m3/3-sum/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ *  class.
+ **/
+public class Solution {
+    /**
+     *  args.
+     *
+     * @param      args  The arguments
+     **/
+    final public static void main(String[] args) {
+    /**
+     *
+     *{ var_description }
+     **/
+        Scanner Scanner = new Scanner(System.in);
+        int arr_size = Scanner.nextInt();
+        int[] arr = new int[arr_size];
+        int count = 0;
+        for (int i = 0; i < arr_size; i++) {
+            arr[i] = Scanner.nextInt();
+        }
+        Arrays.sort(arr);
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < arr_size; i++) {
+            j = i + 1;
+            k = arr_size - 1;
+            while (j < k) {
+                if (arr[i] + arr[j] + arr[k] < 0) {
+                    j += 1;
+                } else if (arr[i] + arr[j] + arr[k] > 0) {
+                    k -= 1;
+                } else {
+                    j += 1;
+                    k -= 1;
+                    count += 1;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
