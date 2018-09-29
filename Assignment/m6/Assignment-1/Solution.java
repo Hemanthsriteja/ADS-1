4067d1cc965c13cb2f626753b192d54dcc25d520
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-29 13:00:24 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- Assignment/m6/Assignment-1/Solution.java
+++ Assignment/m6/Assignment-1/Solution.java
@@ -1,4 +1,10 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
+/**.
+ * Class for add large numbers.
+ */
 class AddLargeNumbers {
     public static LinkedList numberToDigits(String number) {
         LinkedList l = new LinkedList();
@@ -10,15 +16,38 @@ class AddLargeNumbers {
         }
         return l;
     }
+    /**.
+     * { function_description }
+     *
+     * @param      list  The list
+     *
+     * @return     { description_of_the_return_value }
+     */
     public static String digitsToNumber(LinkedList list) {
         return list.displayAll();
     }
+    /**.
+     * Adds large numbers.
+     *
+     * @param      list1  The list 1
+     * @param      list2  The list 2
+     *
+     * @return     { description_of_the_return_value }
+     */
     public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
         return list1;
     }
 }
 
+/**.
+ * Class for solution.
+ */
 public class Solution {
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
