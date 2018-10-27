dc5eab0621ab0a9f3d6ff130e63720a6b7b1afb1
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-10-27 12:13:03 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m24/1/1/Student.java
+++ Assignment/m24/1/1/Student.java
@@ -5,53 +5,52 @@
  * Class for student.
  */
 class Student {
-	/**.
-	 * roll number of the student.
-	 */
-	private int rollnum;
-	/**.
-	 * name of the student.
-	 */
-	private String studentName;
-	/**.
-	 * total marks of the student.
-	 */
-	private double totalmarks;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      rollnum   The roll number
-	 * @param      studentName  The student name
-	 * @param      totalmarks   The total marks
-	 */
-	Student(int rollnum, String studentName, double totalmarks) {
-		this.rollnum = rollnum;
-		this.studentName = studentName;
-		this.totalmarks = totalmarks;
-	}
-	/**.
-	 * Gets the rollnum
-	 *
-	 * @return     The roll.
-	 */
-	public int getRoll() {
-		return this.rollnum;
-	}
-	/**.
-	 * Gets the name.
-	 *
-	 * @return     The name.
-	 */
-	public String getName() {
-		return this.studentName;
-	}
-	/**.
-	 * Gets the total.
-	 *
-	 * @return     The total.
-	 */
-	public double getTotal() {
-		return this.totalmarks;
-	}
+    /**.
+     * roll number of the student.
+     */
+    private int rollnum;
+    /**.
+     * name of the student.
+     */
+    private String studentName;
+    /**.
+     * total marks of the student.
+     */
+    private double totalmarks;
+    /**.
+     * Constructs the object.
+     *
+     * @param      rollnum   The roll number
+     * @param      studentName  The student name
+     * @param      totalmarks   The total marks
+     */
+    Student(final int rollnum, final String studentName, final double totalmarks) {
+        this.rollnum = rollnum;
+        this.studentName = studentName;
+        this.totalmarks = totalmarks;
+    }
+    /**.
+     * Gets the rollnum
+     *
+     * @return     The roll.
+     */
+    public int getRoll() {
+        return this.rollnum;
+    }
+    /**.
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.studentName;
+    }
+    /**.
+     * Gets the total.
+     *
+     * @return     The total.
+     */
+    public double getTotal() {
+        return this.totalmarks;
+    }
 }
-
