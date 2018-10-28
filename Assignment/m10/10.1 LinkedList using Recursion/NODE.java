2666ba6661f20bea7171b8ba5a6f40c30ec54de4
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-10-28 10:48:39 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignment/m10/10.1 LinkedList using Recursion/NODE.java
+++ Assignment/m10/10.1 LinkedList using Recursion/NODE.java
@@ -5,20 +5,20 @@ class Node {
     public Node(){
         link = null;
         data = 0;
-    }    
+    }
     public Node(int d,Node n){
         data = d;
         link = n;
-    }    
+    }
     public void setLink(Node n) {
         link = n;
-    }    
+    }
     public void setData(int d){
         data = d;
-    }    
+    }
     public Node getLink() {
         return link;
-    }    
+    }
     public int getData() {
         return data;
 
